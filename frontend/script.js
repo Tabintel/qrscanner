a14442cbe5bb60ad857dea4963faa1cb4a34f7ff
tabintel <favourekemini@yahoo.com>
2024-08-10 17:00:52 +0100

    update

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ document.querySelector("form").addEventListener("submit", async function(event) 
     formData.append("image", fileField.files[0]);
 
     try {
-        const response = await fetch("http://localhost:8080/upload", {
+        const response = await fetch("https://decode-ohkt.onrender.com/upload", {
             method: "POST",
             body: formData,
         });
