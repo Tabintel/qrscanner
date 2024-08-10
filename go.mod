3a013d198c82996718c3af2a0a08dfebe87d53c0
tabintel <favourekemini@yahoo.com>
2024-08-10 14:49:05 +0100

    update

--- go.mod
+++ go.mod
@@ -3,17 +3,25 @@ module qrscanner
 go 1.22.0
 
 require (
-	github.com/makiuchi-d/gozxing v0.1.1
-	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
+	github.com/swaggo/http-swagger v1.3.4
+	github.com/tuotoo/qrcode v0.0.0-20220425170535-52ccc2bebf5d
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/maruel/rs v1.1.0 // indirect
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
-	github.com/tuotoo/qrcode v0.0.0-20220425170535-52ccc2bebf5d // indirect
-	golang.org/x/sys v0.1.0 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/swaggo/swag v1.8.1 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
--- swag.go
+++ swag.go
@@ -1,4 +1,3 @@
-/*
 package main
 
 import (
@@ -9,12 +8,16 @@ import (
     "os"
 
     "github.com/tuotoo/qrcode"
+    //_ "your_project/docs" // This is to import the Swagger docs
+    "github.com/swaggo/http-swagger" // http-swagger middleware
 )
 
+// QRCodeResult represents the structure of the decoded QR code content
 type QRCodeResult struct {
     URL string `json:"url"`
 }
 
+// DecodeImage decodes the QR code from the image file
 func DecodeImage(imagePath string) ([]QRCodeResult, error) {
     file, err := os.Open(imagePath)
     if err != nil {
@@ -35,6 +38,16 @@ func DecodeImage(imagePath string) ([]QRCodeResult, error) {
     return qrCodes, nil
 }
 
+// uploadHandler handles the image upload and QR code decoding
+// @Summary Upload an image and decode QR codes
+// @Description Upload an image file, decode any QR codes present, and return the decoded URLs
+// @Accept multipart/form-data
+// @Produce json
+// @Param image formData file true "Image file with QR code"
+// @Success 200 {array} QRCodeResult
+// @Failure 400 {string} string "Invalid request"
+// @Failure 500 {string} string "Server error"
+// @Router /upload [post]
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
     if r.Method != "POST" {
         http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
@@ -82,12 +95,19 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
     w.Write(jsonResponse)
 }
 
+// @title QR Code Decoder API
+// @version 1.0
+// @description API for decoding QR codes from images
+// @host localhost:8080
+// @BasePath /
 func main() {
     http.HandleFunc("/upload", uploadHandler)
+
+    // Swagger route
+    http.HandleFunc("/swagger/", httpSwagger.WrapHandler)
+
     fmt.Println("Server started on :8080")
     if err := http.ListenAndServe(":8080", nil); err != nil {
         fmt.Println("Error starting server:", err)
     }
 }
-
-*/
